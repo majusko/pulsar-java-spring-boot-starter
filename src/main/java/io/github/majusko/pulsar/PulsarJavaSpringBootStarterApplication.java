6fc2c5bc54629b0239b09c257df2071e4c38fd72
majusko <mariokapustaa@gmail.com>
2019-11-18 23:47:18 +0000

    Initial project scope.

---
+++ src/main/java/io/github/majusko/pulsar/PulsarJavaSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package io.github.majusko.pulsar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsarJavaSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsarJavaSpringBootStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/majusko/pulsar/PulsarJavaSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.majusko.pulsar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsarJavaSpringBootStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
