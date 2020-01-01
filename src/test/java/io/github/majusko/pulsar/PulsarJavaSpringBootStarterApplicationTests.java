a38b2c6f50b97dd599907b31b2aec37f07690495
majusko <mariokapustaa@gmail.com>
2020-01-01 15:53:29 +0000

    Reformated tests.

--- src/test/java/io/github/majusko/pulsar/PulsarJavaSpringBootStarterApplicationTests.java
+++ src/test/java/io/github/majusko/pulsar/PulsarJavaSpringBootStarterApplicationTests.java
@@ -19,10 +19,10 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -30,63 +30,62 @@ import java.util.stream.Collectors;
 @Import(TestProducerConfiguration.class)
 class PulsarJavaSpringBootStarterApplicationTests {
 
-	@Autowired
-	private ConsumerBuilder consumerBuilder;
+    @Autowired
+    private ConsumerBuilder consumerBuilder;
 
-	@Autowired
-	private ConsumerCollector consumerCollector;
+    @Autowired
+    private ConsumerCollector consumerCollector;
 
-	@Autowired
-	private ProducerFactory producerFactory;
+    @Autowired
+    private ProducerFactory producerFactory;
 
-	@Autowired
-	private PulsarTemplate<MyMsg> producer;
+    @Autowired
+    private PulsarTemplate<MyMsg> producer;
 
-	@Test
-	void testProducerSendMethod() throws PulsarClientException {
-		producer.send("topic-one", new MyMsg("bb"));
-	}
+    @Test
+    void testProducerSendMethod() throws PulsarClientException {
+        producer.send("topic-one", new MyMsg("bb"));
+    }
 
-	@Before
-	public void init() throws InterruptedException {
-		Thread.sleep(100);
-	}
+    @Before
+    public void init() throws InterruptedException {
+        Thread.sleep(100);
+    }
 
-	@Test
-	void testConsumerRegistration1() throws Exception {
-		final List<Consumer> consumers = consumerBuilder.getConsumers();
+    @Test
+    void testConsumerRegistration1() throws Exception {
+        final List<Consumer> consumers = consumerBuilder.getConsumers();
 
-		Assert.assertEquals(1, consumers.size());
+        Assert.assertEquals(1, consumers.size());
 
-		final Consumer consumer = consumers.stream().findFirst().orElseThrow(Exception::new);
+        final Consumer consumer = consumers.stream().findFirst().orElseThrow(Exception::new);
 
-		Assert.assertNotNull(consumer);
-		Assert.assertEquals("mock-topic", consumer.getTopic());
-	}
+        Assert.assertNotNull(consumer);
+        Assert.assertEquals("mock-topic", consumer.getTopic());
+    }
 
-	@Test
-	void testConsumerRegistration2() throws Exception {
-		final Class<TestConsumerConfiguration> clazz = TestConsumerConfiguration.class;
-		final String descriptor = clazz.getName() + "#" + clazz.getDeclaredMethods()[0].getName();
-		final ConsumerHolder consumerHolder = consumerCollector.getConsumer(descriptor).orElseThrow(Exception::new);
+    @Test
+    void testConsumerRegistration2() throws Exception {
+        final Class<TestConsumerConfiguration> clazz = TestConsumerConfiguration.class;
+        final String descriptor = clazz.getName() + "#" + clazz.getDeclaredMethods()[0].getName();
+        final ConsumerHolder consumerHolder = consumerCollector.getConsumer(descriptor).orElseThrow(Exception::new);
 
-		Assert.assertNotNull(consumerHolder);
-		Assert.assertEquals("mock-topic", consumerHolder.getAnnotation().topic());
-		Assert.assertEquals(TestConsumerConfiguration.class, consumerHolder.getBean().getClass());
-		Assert.assertEquals("mockTheListener", consumerHolder.getHandler().getName());
-	}
+        Assert.assertNotNull(consumerHolder);
+        Assert.assertEquals("mock-topic", consumerHolder.getAnnotation().topic());
+        Assert.assertEquals(TestConsumerConfiguration.class, consumerHolder.getBean().getClass());
+        Assert.assertEquals("mockTheListener", consumerHolder.getHandler().getName());
+    }
 
-	@Test
-	void testProducerRegistration() {
+    @Test
+    void testProducerRegistration() {
 
-		final Map<String, ImmutablePair<Class<?>, Serialization>> topics = producerFactory.getTopics();
+        final Map<String, ImmutablePair<Class<?>, Serialization>> topics = producerFactory.getTopics();
 
-		Assert.assertEquals(2, topics.size());
+        Assert.assertEquals(2, topics.size());
 
-		final Set<String> topicNames = topics.entrySet().stream()
-				.map(Map.Entry::getKey).collect(Collectors.toSet());
+        final Set<String> topicNames = new HashSet<>(topics.keySet());
 
-		Assert.assertTrue(topicNames.contains("topic-one"));
-		Assert.assertTrue(topicNames.contains("topic-two"));
-	}
+        Assert.assertTrue(topicNames.contains("topic-one"));
+        Assert.assertTrue(topicNames.contains("topic-two"));
+    }
 }
