6d9fce34be4c52f946c2a002e74e4886c58462a5
majusko <mariokapustaa@gmail.com>
2021-08-23 15:30:05 +0200

    Added configuration support for TLS.

--- src/main/java/io/github/majusko/pulsar/properties/PulsarProperties.java
+++ src/main/java/io/github/majusko/pulsar/properties/PulsarProperties.java
@@ -2,6 +2,9 @@ package io.github.majusko.pulsar.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @ConfigurationProperties(prefix = "pulsar")
 public class PulsarProperties {
     private String serviceUrl = "pulsar://localhost:6650";
@@ -16,6 +19,15 @@ public class PulsarProperties {
     private String consumerNameDelimiter = "";
     private String namespace = "default";
     private String tenant = "public";
+    private String tlsTrustCertsFilePath = null;
+    private Set<String> tlsCiphers = new HashSet<>();
+    private Set<String> tlsProtocols = new HashSet<>();
+    private String tlsTrustStorePassword = null;
+    private String tlsTrustStorePath = null;
+    private String tlsTrustStoreType = null;
+    private boolean useKeyStoreTls = false;
+    private boolean allowTlsInsecureConnection = false;
+    private boolean enableTlsHostnameVerification = false;
 
     public String getServiceUrl() {
         return serviceUrl;
@@ -111,5 +123,77 @@ public class PulsarProperties {
 
     public void setTenant(String tenant) {
         this.tenant = tenant;
+    }
+
+    public String getTlsTrustCertsFilePath() {
+        return tlsTrustCertsFilePath;
+    }
+
+    public void setTlsTrustCertsFilePath(String tlsTrustCertsFilePath) {
+        this.tlsTrustCertsFilePath = tlsTrustCertsFilePath;
+    }
+
+    public Set<String> getTlsCiphers() {
+        return tlsCiphers;
+    }
+
+    public void setTlsCiphers(Set<String> tlsCiphers) {
+        this.tlsCiphers = tlsCiphers;
+    }
+
+    public Set<String> getTlsProtocols() {
+        return tlsProtocols;
+    }
+
+    public void setTlsProtocols(Set<String> tlsProtocols) {
+        this.tlsProtocols = tlsProtocols;
+    }
+
+    public String getTlsTrustStorePassword() {
+        return tlsTrustStorePassword;
+    }
+
+    public void setTlsTrustStorePassword(String tlsTrustStorePassword) {
+        this.tlsTrustStorePassword = tlsTrustStorePassword;
+    }
+
+    public String getTlsTrustStorePath() {
+        return tlsTrustStorePath;
+    }
+
+    public void setTlsTrustStorePath(String tlsTrustStorePath) {
+        this.tlsTrustStorePath = tlsTrustStorePath;
+    }
+
+    public String getTlsTrustStoreType() {
+        return tlsTrustStoreType;
+    }
+
+    public void setTlsTrustStoreType(String tlsTrustStoreType) {
+        this.tlsTrustStoreType = tlsTrustStoreType;
+    }
+
+    public boolean isUseKeyStoreTls() {
+        return useKeyStoreTls;
+    }
+
+    public void setUseKeyStoreTls(boolean useKeyStoreTls) {
+        this.useKeyStoreTls = useKeyStoreTls;
+    }
+
+    public boolean isAllowTlsInsecureConnection() {
+        return allowTlsInsecureConnection;
+    }
+
+    public void setAllowTlsInsecureConnection(boolean allowTlsInsecureConnection) {
+        this.allowTlsInsecureConnection = allowTlsInsecureConnection;
+    }
+
+    public boolean isEnableTlsHostnameVerification() {
+        return enableTlsHostnameVerification;
+    }
+
+    public void setEnableTlsHostnameVerification(boolean enableTlsHostnameVerification) {
+        this.enableTlsHostnameVerification = enableTlsHostnameVerification;
     }
 }
